969a952cb704c7e5cabe65994b9a3446db14efe2
Edwin Muñoz Gaviria <edwin-mg@hotmail.com>
2017-08-27 07:41:06 -0500

    Estado video cuetro de la palicaciòn ForeingEx..

---
+++ foreingchangemvvmmac/Models/Rate.cs
@@ -0,0 +1,17 @@
+﻿
+namespace foreingchangemvvmmac.Models
+{
+    using System;
+
+    public class Rate
+    {
+        public int RateId { get;set;}
+
+        public string Code { get; set;}
+
+        public double TaxRate { get; set;}
+
+        public string Name { get; set;}
+
+    }
+}
--- foreingchangemvvmmac/ViewModels/MainViewModel.cs
+++ foreingchangemvvmmac/ViewModels/MainViewModel.cs
@@ -2,9 +2,79 @@
 namespace foreingchangemvvmmac.ViewModels
 {
     using System;
+    using Models;
+    using System.Collections.ObjectModel;
+    using System.Windows.Input;
+    using GalaSoft.MvvmLight.Command;
 
     public class MainViewModel
     {
+        
+        #region Properties
+
+        public string Amount
+        {
+            get;
+            set;
+        }
+
+        public ObservableCollection<Rate> Rates
+        {
+            get;
+            set;
+        }
+
+        public Rate SourceRate
+        {
+            get;
+            set;
+       }
+
+        public Rate TragetRate
+        {
+            get;
+            set;
+        }
+
+        public bool IsRunning
+        {
+            get;
+            set;
+        }
+
+        public bool IsEnabled
+        {
+            get;
+            set;
+        }
+
+        public string Result
+        {
+            get;
+            set;
+        }
+
+        #endregion
+
+
+        #region Comandos
+
+        public ICommand ConvertCommand
+        {
+            get
+            {
+                return new RelayCommand(Convert);
+            }
+
+        }
+
+        void Convert()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
         public MainViewModel()
         {
             
